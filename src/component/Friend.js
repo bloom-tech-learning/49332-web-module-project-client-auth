494c3cb29eee2acc777af57d83c25addab5c697a
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-14 21:10:38 -0800

    Build the friendslist component

--- src/component/Friend.js
+++ src/component/Friend.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./../CSS/Friend.css";
 
-const Friend = () => {
-
+const Friend = (props) => {
+    const { friend } = props
     return (
-        <div>
-
+        <div className="friends-container">
+            <div className="friend-card"></div>
+                <div className="plant-card">
+                <h3>Friend Info</h3>
+                <p>name: {friend.name}</p>
+                <p>Age: {friend.age}</p>
+                <p>email: {friend.email}</p>
+                
+                </div>
         </div>
     )
 }
--- src/component/FriendList.js
+++ src/component/FriendList.js
@@ -1,10 +1,36 @@
 import React from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import Friend from './Friend'
 
 class FriendList extends React.Component {
+    state = {
+        friends: []
+    }
+    
+    componentDidMount() {
+        axiosWithAuth()
+        .get ('/friends')        
+        .then (resp => {
+            console.log('resp in FriendsList: ', resp);
+            this.setState({
+                ...this.state,
+                friends: resp.data
+              })                 
+        })
+        .catch (err => console.log(err))
+    };
 
     render () {
         return (
             <div>
+                
+                {
+                    this.state.friends.map(friend => {
+                        return (
+                            <Friend key={friend.id} friend={friend}  />
+                        )
+                    })
+                }
 
             </div>
         )
